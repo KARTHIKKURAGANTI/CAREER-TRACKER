247ef51d46f33da96d6d8013046a463df4e4924f
KURAGANTI LAKSHMI KARTHIK <urstrulykartheeek@gmail.com>
2026-03-20 00:21:56 +0530

    Update Dashboard component layout
    Refactor Dashboard component layout with div wrappers for better structure and also added<outlet> for changing child components instead of reloading whole page.

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,21 +1,29 @@
 import { Outlet } from "react-router-dom";
-
 import "./Dashboard.css"
 import Sidebar from "./Sidebar"
 import Topbar from "./Topbar"
 
 function Dashboard(){
-
   return(
-    <>
-    <Sidebar/>
-    <Topbar/>
-    <Outlet/>
-    </>
-    
-    
-  )
+    <div className="dashboard-layout">
 
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main Area */}
+      <div className="dashboard-main">
+
+        {/* Topbar */}
+        <Topbar />
+
+        {/* Dynamic Content */}
+        <div className="dashboard-content">
+          <Outlet />
+        </div>
+
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard
