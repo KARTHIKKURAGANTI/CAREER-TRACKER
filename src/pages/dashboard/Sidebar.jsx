a48071ea65eca4fcfb8a814b2a9dba1cd33f019b
KURAGANTI LAKSHMI KARTHIK <urstrulykartheeek@gmail.com>
2026-03-20 00:24:24 +0530

    Refactor Sidebar component with updated links and adjusted styles as per dashboard

--- src/pages/dashboard/Sidebar.jsx
+++ src/pages/dashboard/Sidebar.jsx
@@ -1,28 +1,22 @@
 import { Link } from "react-router-dom"
-/*import "./Sidebar.css"*/
+import "./Sidebar.css"
 
 function Sidebar(){
-
   return(
-
     <div className="sidebar">
 
       <h2>Career <span style={{color:"white"}}>Tracker</span></h2>
 
       <ul>
-
-        <li>Dashboard</li>
-        <Link to="/analytics"><li>Analytics</li></Link>
-        <Link to="/integrations"><li>Integrations</li></Link>
-        <li>Profile</li>
-        <li>Settings</li>
-
+        <li><Link to="Dhome">Dashboard Home</Link></li>
+        <li><Link to="analytics">Analytics</Link></li>
+        <li><Link to="integrations">Integrations</Link></li>
+        <li><Link to="profile">Profile</Link></li>
+        <li><Link to="settings">Settings</Link></li>
       </ul>
 
     </div>
-
   )
-
 }
 
 export default Sidebar
